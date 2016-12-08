e97d84cb3053ae701caadaf0d2dfa595169a03e8
Robert Costa <zabbarob@gmail.com>
2016-12-08 05:41:17 +0100

    handle request to get todo by id

---
+++ api/controllers/FindTodoById.js
@@ -0,0 +1,21 @@
+'use strict';
+var todos = require('../helpers/db');
+
+module.exports = {
+    FindTodoById: FindTodoById
+}
+
+function FindTodoById(req, res) {
+    var id = req.swagger.params.id.value;
+    var requestedTodo = todos.find(todo => todo.todo_id === id);
+
+    res.header('Content-Type', 'application/json');
+
+    var response = requestedTodo;
+    if (!response) {
+        response = { error: 'Error: Todo doesn\'t exist' };
+        console.log(response.error);
+    }
+
+    res.end(JSON.stringify(response));
+}
