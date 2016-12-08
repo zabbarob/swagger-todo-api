7bf6058be3a8001513a14f59b16e4cc09da5be86
Robert Costa <zabbarob@gmail.com>
2016-12-08 05:21:50 +0100

    fix wrong property name in todo db

--- api/helpers/db.js
+++ api/helpers/db.js
@@ -4,14 +4,14 @@ module.exports = [{
     todo_id: 1,
     todo: "Get some real milk",
     author: "The real Jim",
-    createddate: "2016-11-01T23:15:00.000Z",
+    datecreated: "2016-11-01T23:15:00.000Z",
     duedate: "2016-11-08T08:00:00.000Z",
     completed: false
 }, {
     todo_id: 2,
     todo: "Get some real cereal",
     author: "The real Austin",
-    createddate: "2016-11-01T23:15:00.000Z",
+    datecreated: "2016-11-01T23:15:00.000Z",
     duedate: "2016-11-08T08:00:00.000Z",
     completed: false
 }];
